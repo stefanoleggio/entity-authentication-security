9036d13cc243c74e22ebdf6c8a081d4c536c1e52
stefano28 <stefanoleggio28@gmail.com>
2021-12-15 15:06:58 +0100

    adding comments

--- authentication.py
+++ authentication.py
@@ -1,24 +1,41 @@
 from prover import Prover
 from verifier import Verifier
+import time
 
-p = 11
-a = 2
+def authentication(p,a,k):
+    A = Prover(p,a,k)
+    B = Verifier(p,a,A.k_1)
+    B.generate_challenge()
+    t = A.compute_tags(B.c)
+    B.authenticate(t)
 
-k = 4 # Private key of A
 
+if __name__ == "__main__":
 
-A = Prover(p,a,k)
+    print("\nAuthentication protocol test")
 
-B = Verifier(p,a,A.k_1)
+    p = 11
+    a = 2
+    k = 4 # Private key of A
 
-B.generate_challenge()
+    A = Prover(p,a,k)
 
-t = A.compute_tags(B.c)
+    B = Verifier(p,a,A.k_1) # In this case B knows the public key of A
 
-print("p: ", p)
-print("a: ",a)
-print("c: ", B.c)
-if(B.authenticate(t)):
-    print("A is authenticated by B")
-else:
-    print("A is rejected by B")
+    B.generate_challenge()
+
+    t = A.compute_tags(B.c)
+    print("\nSetup variables")
+    print(" p: ", p)
+    print(" a: ",a)
+    print(" c: ", B.c)
+    print("\nKeys of A")
+    print(" k: ", k, "(private key)")
+    print(" k_1: ", A.k_1, "(public key)")
+    print("\nResult: ")
+    if(B.authenticate(t)):
+        print(" A is authenticated by B")
+    else:
+        print(" A is rejected by B")
+
+    print("\nEvaluate its running time for several values of p between 10^3 and 10^7\n")
